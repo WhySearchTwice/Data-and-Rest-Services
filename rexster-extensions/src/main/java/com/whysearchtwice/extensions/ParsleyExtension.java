750a2b8ead08d92a1a47757fbae699f4aac8b63b
Tony Grosinger <tony@grosinger.net>
2013-01-30 21:33:39 -0800

    Refactored to reduce redundancy, changed namespace, added update vertex endpoint

--- rexster-extensions/src/main/java/com/whysearchtwice/extensions/ParsleyExtension.java
+++ rexster-extensions/src/main/java/com/whysearchtwice/extensions/ParsleyExtension.java
@@ -22,30 +22,76 @@ import com.tinkerpop.rexster.extension.RexsterContext;
 @ExtensionNaming(name = "vertex", namespace = "parsley")
 public class ParsleyExtension extends AbstractRexsterExtension {
 
-    @ExtensionDefinition(extensionPoint = ExtensionPoint.GRAPH, path = "createVertex", method = HttpMethod.POST)
+    @ExtensionDefinition(extensionPoint = ExtensionPoint.GRAPH, method = HttpMethod.POST)
     @ExtensionDescriptor(description = "create a new vertex in the graph")
-    public ExtensionResponse doSomeWorkOnGraph(@RexsterContext RexsterResourceContext context, @RexsterContext Graph graph) {
+    public ExtensionResponse createNewVertex(@RexsterContext RexsterResourceContext context, @RexsterContext Graph graph) {
         JSONObject attributes = context.getRequestObject();
-        Iterator keysIter = attributes.keys();
-
-        // Map to store the results
-        Map<String, String> map = new HashMap<String, String>();
 
         // Create the new Vertex
         Vertex newVertex = graph.addVertex(null);
+        updateVertexProperties(newVertex, attributes);
+
+        // Return the id of the new Vertex
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("id", newVertex.getId().toString());
+
+        // Create an edge to the Predecessor or Parent if needed
+        try {
+            if (attributes.has("predecessor")) {
+                boolean result = createEdge(graph, newVertex, attributes.getInt("predecessor"), "successorTo", "predecessorTo");
+                map.put("predecessor", (result) ? "predecessor created successfully" : "predecessor could not be created");
+            }
+            if (attributes.has("parent")) {
+                boolean result = createEdge(graph, newVertex, attributes.getInt("parent"), "childOf", "parentOf");
+                map.put("parent", (result) ? "parent created successfully" : "parent could not be created");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return ExtensionResponse.ok(map);
+    }
+
+    @ExtensionDefinition(extensionPoint = ExtensionPoint.VERTEX, method = HttpMethod.POST)
+    @ExtensionDescriptor(description = "update an existing vertex in the graph")
+    public ExtensionResponse updateVertex(@RexsterContext RexsterResourceContext context, @RexsterContext Vertex vertex) {
+        updateVertexProperties(vertex, context.getRequestObject());
+
+        // Map to store the results
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("message", "vertex updated");
+
+        return ExtensionResponse.ok(map);
+    }
+
+    private boolean createEdge(Graph graph, Vertex v1, int v2id, String message1, String message2) {
+        Vertex v2 = graph.getVertex(v2id);
+        if (v2 != null) {
+            graph.addEdge(null, v1, v2, message1);
+            graph.addEdge(null, v2, v1, message2);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void updateVertexProperties(Vertex v, JSONObject attributes) {
+        Iterator keysIter = attributes.keys();
+
+        // For any property that exists in the map, update it
         while (keysIter.hasNext()) {
             try {
                 String key = (String) keysIter.next();
 
                 if (key.equals("type") || key.equals("pageUrl") || key.equals("userId") || key.equals("deviceId")) {
                     String value = (String) attributes.get(key);
-                    newVertex.setProperty(key, value);
+                    v.setProperty(key, value);
                 } else if (key.equals("pageOpenTime") || key.equals("pageCloseTime")) {
                     Long value = (Long) attributes.get(key);
-                    newVertex.setProperty(key, value);
+                    v.setProperty(key, value);
                 } else if (key.equals("tabId") || key.equals("windowId")) {
                     int value = (Integer) attributes.get(key);
-                    newVertex.setProperty(key, value);
+                    v.setProperty(key, value);
                 } else {
                     // Ignore the property for now
                 }
@@ -53,41 +99,5 @@ public class ParsleyExtension extends AbstractRexsterExtension {
                 e1.printStackTrace();
             }
         }
-
-        // Return the id of the new Vertex
-        map.put("id", newVertex.getId().toString());
-
-        // Create an edge to the Predecessor if needed
-        if (attributes.has("predecessor")) {
-            try {
-                Vertex predecessorVertex = graph.getVertex(attributes.get("predecessor"));
-                if (predecessorVertex != null) {
-                    System.out.println("Creating the predecessor edges");
-                    graph.addEdge(null, newVertex, predecessorVertex, "successorTo");
-                    graph.addEdge(null, predecessorVertex, newVertex, "predecessorTo");
-                } else {
-                    map.put("error", "could not find predecessor");
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        // Create an edge to the parent if needed
-        if (attributes.has("parent")) {
-            try {
-                Vertex parentVertex = graph.getVertex(attributes.get("parent"));
-                if (parentVertex != null) {
-                    graph.addEdge(null, newVertex, parentVertex, "childOf");
-                    graph.addEdge(null, parentVertex, newVertex, "parentOf");
-                } else {
-                    map.put("error", "could not find parent");
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return ExtensionResponse.ok(map);
     }
 }
